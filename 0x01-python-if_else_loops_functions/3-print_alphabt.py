03aa9dee7df579fc3cb337c270b75fa106e0528d
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-24 03:47:27 +0100

    display with format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphabet in "abcdefghijklmnopqrstuvwxyz":
     if alphabet != 'q' and alphabet != 'e':
-        print(alphabet, end="")
+        print("{}".format(alphabet), end="")
