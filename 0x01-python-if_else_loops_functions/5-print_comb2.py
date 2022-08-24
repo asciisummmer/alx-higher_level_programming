a7ca30121f7daff47e028d5b004c916bb278f219
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-24 03:54:49 +0100

    print digit on two position

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 for num in range(100):
     if num != 99:
-        print("{}, ".format(num), end="")
+        if num > 9:
+            output = "{}, "
+        else:
+            output = "0{}, "
+        print(output.format(num), end="")
     else:
         print(num)
