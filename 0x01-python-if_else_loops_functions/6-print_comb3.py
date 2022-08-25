cf651aa0b96ba8dc7d7a0ab2584ae271960b614a
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-25 01:53:52 +0100

    fix pycodestyle error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num_one in range(9):
-    for num_two in range(num_one + 1,10):
+    for num_two in range(num_one + 1, 10):
         if num_one != 8 or num_two != 9:
             print("{}{}, ".format(num_one, num_two), end="")
         else:
