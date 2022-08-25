297ae5956f5b87d59a18d0fd554951840bb950c8
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-25 02:37:58 +0100

    optimize function uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     for value in str:
         ascii_value = ord(value)
+        char = value
         if 122 >= ascii_value >= 97:
-            print("{}".format(chr(ascii_value - 32)), end="")
-        else:
-            print("{}".format(value), end="")
+            char = chr(ascii_value - 32)
+        print("{}".format(char), end="")
     print()
