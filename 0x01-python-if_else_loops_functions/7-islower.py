cb6517796a1304d7bd1839cd76ba280c2c600a8f
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-25 02:11:00 +0100

    fix error comparison

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     ascii_value = ord(c)
-    if 122 >= ascii_value > 97:
+    if 122 >= ascii_value >= 97:
         return True
     else:
         return False
