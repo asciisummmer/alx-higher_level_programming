1bb4cba44e0eaae1f79bc93a442061a017766170
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-25 02:24:39 +0100

    fix error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for value in str:
         ascii_value = ord(value)
         if 122 >= ascii_value >= 97:
-            print(chr(ascii_value - 32), end="")
+            print("{}".format(chr(ascii_value - 32)), end="")
         else:
-            print(value, end="")
+            print("{}".format(value), end="")
     print()
