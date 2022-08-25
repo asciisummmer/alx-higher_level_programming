fc5b2fa65b055971cb5cc96fdeb55b429d62e472
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-25 03:27:33 +0100

    print reverse alpha low an upper case

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_tebahpla():
+    value = ord('z')
+    while(value >= ord('a')):
+        if value % 2 == 0:
+            value_to_print = chr(value)
+        else:
+            value_to_print = chr(value - 32)
+        print(value_to_print, end="")
+        value -= 1
+
+print_tebahpla()
