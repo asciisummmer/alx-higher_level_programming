b3f910ae73a58e60fd3b7b31efb21616ba400094
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-25 03:35:43 +0100

    fix pycodestyle

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,12 +2,13 @@
 
 def print_tebahpla():
     value = ord('z')
-    while(value >= ord('a')):
+    while value >= ord('a'):
         if value % 2 == 0:
             value_to_print = chr(value)
         else:
             value_to_print = chr(value - 32)
         print("{}".format(value_to_print), end="")
         value -= 1
+
 
 print_tebahpla()
