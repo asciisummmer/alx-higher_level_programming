c8621515d4e62305f342657e83c36ddb79b6e29e
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-25 01:50:59 +0100

    print comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for num in range(100):
-    if num != 99:
-        print(f"{num}, ", end="")
-    else:
-        print(num)
+for num_one in range(9):
+    for num_two in range(num_one + 1,10):
+        if num_one != 8 or num_two != 9:
+            print(f"{num_one:d}{num_two:d}, ", end="")
+        else:
+            print(f"{num_one:d}{num_two:d}")
