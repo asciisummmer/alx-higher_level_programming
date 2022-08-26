17cf6a56b20a840ccff17338fc8e9f78dec5973f
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-26 03:49:46 +0100

    fix error name variable

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import hidden_4
 
     symboles = dir(hidden_4)
-    for symbole in symbole:
+    for symbole in symboles:
         if symbole[:2] != "__":
             print(symbole)
