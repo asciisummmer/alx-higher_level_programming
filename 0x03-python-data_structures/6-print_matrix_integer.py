35fb26a4edfe18043c10b6ec7e831b2a2b800b4f
asciisummer <elwis.soviguidi@epitech.eu>
2022-08-31 16:26:16 +0100

    fix error cast

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for i in range(0, len(line)):
             if i + 1 == len(line):
-                print(line[i], end="")
+                print("{:d}".format(line[i]), end="")
             else:
-                print(line[i], end=" ")
+                print("{:d}".format(line[i]), end=" ")
         print()
