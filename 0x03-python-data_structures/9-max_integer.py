0201ea6a633dd4e1fed042d786ccde3d7c5531e6
asciisummer <elwis.soviguidi@epitech.eu>
2022-09-05 00:09:37 +0100

    get max integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = my_list[0]
+    if len(list) == 0:
+        return None
+    for i in range(1, len(my_list)):
+        if my_list[i] > max:
+            max = my_list[i]
+    return max
