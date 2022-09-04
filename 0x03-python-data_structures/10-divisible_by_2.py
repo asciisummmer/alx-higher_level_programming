b57cea82674f99876e5705a8316df7f94954d884
asciisummer <elwis.soviguidi@epitech.eu>
2022-09-05 00:26:05 +0100

    fix error list null and check number is divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    number_divisible_2 = []
+    for elt in my_list:
+        if elt % 2 == 0:
+            number_divisible_2.append(True)
+        else:
+            number_divisible_2.append(False)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = my_list[0]
-    if len(list) == 0:
+    if len(my_list) == 0:
         return None
     for i in range(1, len(my_list)):
         if my_list[i] > max:
