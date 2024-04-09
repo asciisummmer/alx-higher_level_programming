d8a116ba4a1c3733f2dbe071812a9a7b22c7d5a7
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-09 20:20:21 +0100

    fix coding style

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,13 +5,17 @@
 class Square:
     """ Class to create Square """
     def __init__(self, size=0):
+        """ Create new instance of square
 
+        Args:
+            size(int): Size of square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         return self.__size ** 2
