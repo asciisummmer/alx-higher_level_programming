f8c9bd96a08bab3d50b39f89295fa25d7be526e4
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-09 01:05:42 +0100

    add rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Module to describe Rectangle """
+
+
+class Rectangle:
+    """ Class to describe Rectangle """
+    def __init__(self):
+        pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+# Learn python programming by exercices
+
+## More about Classe and object
