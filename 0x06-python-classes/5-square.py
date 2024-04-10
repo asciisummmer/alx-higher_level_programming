f5ce0b2de1860799a53fa6bc406d897f9593bc54
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-10 18:47:58 +0100

    fix string error for position tuple

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,24 +4,13 @@
 
 class Square:
     """ Class to create Square """
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0):
         self.size = size
-        self.position = position
         """ Create new instance of square
 
         Args:
             size(int): Size of square
         """
-
-    @property
-    def position(self):
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     @property
     def size(self):
