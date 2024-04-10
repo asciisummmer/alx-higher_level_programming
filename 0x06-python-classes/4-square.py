fbff867ab87a07412baa08e1109b546fd863973c
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-10 18:05:04 +0100

    define property on size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module to create Square"""
+
+
+class Square:
+    """ Class to create Square """
+    def __init__(self, size=0):
+        self.size = size
+        """ Create new instance of square
+
+        Args:
+            size(int): Size of square
+        """
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
