acbd77d3b9d3c1d1d41210c70c21f681e2eb556b
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-13 16:14:32 +0100

    import many functions from module

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, mul, div, sub
+
+if __name__ == "__main__":
+    a = 10
+    b = 15
+    print("{:d} + {:d} = {}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {}".format(a, b, div(a, b)))
