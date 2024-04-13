98e094bf5740c8b2d58f73fa89a778b6ada34b0e
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-13 17:47:38 +0100

    add somme extra values

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,22 +8,17 @@ if __name__ == "__main__":
     if len_arg != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    result = 0
     a = int(argv[1])
     b = int(argv[3])
     match argv[2]:
         case "+":
-            result = a + b
-            print("{} + {} = {}".format(a, b, add(a, b)))
+            print("{:d} + {:d} = {}".format(a, b, add(a, b)))
         case "-":
-            result = a - b
-            print("{} - {} = {}".format(a, b, sub(a, b)))
+            print("{:d} - {:d} = {}".format(a, b, sub(a, b)))
         case "*":
-            result = a * b
-            print("{} * {} = {}".format(a, b, mul(a, b)))
+            print("{:d} * {:d} = {}".format(a, b, mul(a, b)))
         case "/":
-            result = a / b
-            print("{} / {} = {}".format(a, b, div(a, b)))
+            print("{:d} / {:d} = {}".format(a, b, div(a, b)))
         case _:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
