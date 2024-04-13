43303a9606599b38421332f491e33060b20a6bc4
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-13 17:36:38 +0100

    cast integer a and b

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,19 +8,20 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     result = 0
-
+    a = int(argv[1])
+    b = int(argv[3])
     match argv[2]:
         case "+":
-            result = int(argv[1]) + int(argv[3])
+            result = a + b
             print("{} + {} = {}".format(argv[1], argv[3], result))
         case "-":
-            result = int(argv[1]) - int(argv[3])
+            result = a - b
             print("{} - {} = {}".format(argv[1], argv[3], result))
         case "*":
-            result = int(argv[1]) * int(argv[3])
+            result = a * b
             print("{} * {} = {}".format(argv[1], argv[3], result))
         case "/":
-            result = int(argv[1]) / int(argv[3])
+            result = a / b
             print("{} / {} = {}".format(argv[1], argv[3], result))
         case _:
             print("Unknown operator. Available operators: +, -, * and /")
