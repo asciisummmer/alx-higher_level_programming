8d2de6537707514ab02a7f526497aaf6bde56d01
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-13 16:16:43 +0100

    fix error value of b

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ from calculator_1 import add, mul, div, sub
 
 if __name__ == "__main__":
     a = 10
-    b = 15
+    b = 5
     print("{:d} + {:d} = {}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {}".format(a, b, mul(a, b)))
