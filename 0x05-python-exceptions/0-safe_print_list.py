df88ca68a721f963bdbbc67403f65deed0801715
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-14 19:40:01 +0100

    print integer

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    if x == 0 or my_list == []:
+        return 0
     try:
         for i in range(x):
             print(my_list[i], end="")
