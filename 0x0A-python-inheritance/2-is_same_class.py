981b56e564d0e2387ceb4b8a0dbdc2af37d606d9
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-16 00:24:40 +0100

    verify class of object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module to use isinstance function"""
+
+
+def is_same_class(obj, a_class):
+    """ Verify class of object
+
+    Args:
+        obj: object to verify class
+        a_class: class to verify
+
+    Returns:
+        True if is same class otherwise false
+    """
+    return isinstance(obj, a_class)
