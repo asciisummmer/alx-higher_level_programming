25341aa73b2990ea5cd01a52864108001840dbc7
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-16 01:14:24 +0100

    raise Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to inherit class."""
+
+
+class BaseGeometry:
+    """Define class."""
+    def area(self):
+        """Methode to compute area
+
+        Raises:
+            Exception: not implemented
+        """
+        raise Exception("area() is not implemented")
