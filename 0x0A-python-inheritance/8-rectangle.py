8f11a56ec28aeae890dc80cddc69e7f79fdcd24d
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-17 20:03:24 +0100

    fix errors super call

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,29 +13,5 @@ class Rectangle(BaseGeometry):
     __height = 0
 
     def __init__(self, width, height):
-        __width = self.integer_validator(width, "width")
-        __height = self.integer_validator(height, "height")
-
-    def integer_validator(self, value, name):
-        """ Verify value is integer and greather than zero
-
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
-
-        Args:
-            value (int): value to check
-            name (str): name of variable of value
-
-        Returns:
-            return value if integr greather than zero
-
-        Raises:
-            TypeError: if value not integer
-            ValueError: if value less than zero
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value < 0:
-            raise ValueError("{} must be positive integer".format(name))
-        else:
-            return value
+        __width = super().integer_validator(width, "width")
+        __height = self().integer_validator(height, "height")
