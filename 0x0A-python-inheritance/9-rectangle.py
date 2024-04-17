16103a5c6d103755f6b3afe2d46867731b2f5769
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-17 20:45:26 +0100

    compute area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module to define Rectangle class."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Module to represent Rectangle
+
+    Attributes:
+        __width (int): width of rectangle
+        __height (int): height of rectangle
+    """
+
+    def __init__(self, width, height):
+        """Create new square
+
+        Args:
+            width (int): width of Rectangle
+            height (int): height of Rectangle
+        """
+
+        self.__width = BaseGeometry.integer_validator(self, "width", width)
+        self.__height = BaseGeometry.integer_validator(self, "height", height)
+
+    def area(self):
+        """Methode to compute area
+
+        Return:
+            area of Rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
