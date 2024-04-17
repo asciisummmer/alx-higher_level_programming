5df23d74bc78ba75dcc46d333c0b5a880fa1661c
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-17 20:24:38 +0100

    add super

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,5 +11,7 @@ class Rectangle(BaseGeometry):
     """
 
     def __init__(self, width, height):
-        self.__width = super().integer_validator("width", width)
-        self.__height = super().integer_validator("height", height)
+        
+        self.__width = BaseGeometry.integer_validator(self, "width", width)
+        self.__height = BaseGeometry.integer_validator(self, "height", height)
+        super().__init__()
