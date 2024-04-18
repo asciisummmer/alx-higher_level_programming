85930cb74f0cf7a10eef015afdd9bc1c69a7234b
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-18 03:40:08 +0100

    write json in file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Module to load json"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function to write in file
+
+    Args:
+        filename (str): path of file "".
+        my_obj (str): object to write in file.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
