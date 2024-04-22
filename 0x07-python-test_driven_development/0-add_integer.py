cb7c568e1077b77dad6e423d231f21795bda522e
Elwis SOVIGUIDI <romelsoviguidi@gmail.com>
2024-04-22 17:58:35 +0100

    fix error float is nan

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,13 +6,23 @@
     function add_integer
 """
 
+
 def add_integer(a, b=98):
     """Return add of a and b.
 
     """
-    if type(a) is not int and type(a) is not float:
+    try:
+        if type(a) is not int and type(a) is not float:
+            raise TypeError
+        a = int(a)
+    except (ValueError, TypeError):
         raise TypeError("a must be an integer")
 
-    if type(b) is not int and type(b) is not float:
+    try:
+        if type(b) is not int and type(b) is not float:
+            raise TypeError
+        b = int(b)
+    except (ValueError, TypeError):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+
+    return a + b
