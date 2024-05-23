f21272fe87b69e78f42ffd0ce0e5191a74add59f
asciisummer <romelsoviguidi@gmail.com>
2024-05-23 03:54:11 +0100

    fix: sql declaration in UPPERCASE

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of California 
 -- lists all the cities of California
-SELECT * FROM cities, states where states.name = "California" AND cities.state_id = states.id;
+SELECT * FROM cities, states WHERE states.name = "California" AND cities.state_id = states.id ORDER BY cities.id ASC;
