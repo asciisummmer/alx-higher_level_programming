27165b9c13bc2e0e5b590baa2c029e6063c8ca4f
asciisummer <romelsoviguidi@gmail.com>
2024-05-23 03:59:58 +0100

    ff

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of California 
 -- lists all the cities of California
-SELECT * FROM cities, states WHERE states.name = "California" AND cities.state_id = states.id ORDER BY cities.id ASC;
+SELECT * FROM cities WHERE states.name = "California" AND cities.state_id = states.id ORDER BY cities.id ASC;
