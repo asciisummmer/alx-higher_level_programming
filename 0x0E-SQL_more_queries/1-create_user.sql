74cb01c6a85c65e27f10df549a0af701a4144ac3
asciisummer <romelsoviguidi@gmail.com>
2024-05-23 01:50:28 +0100

    feat: create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates the MySQL server user
+-- creates the MySQL server user
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'user_0d_1_PWD';
+FLUSH PRIVILEGES;
