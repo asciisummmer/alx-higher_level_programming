75cb60d877f571154de859d653fbee62c9ede5b2
asciisummer <romelsoviguidi@gmail.com>
2024-05-23 01:57:44 +0100

    fix: change server name

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates the MySQL server user
 -- creates the MySQL server user
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'user_0d_1_PWD';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
