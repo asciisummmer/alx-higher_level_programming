806c11abe90c95ca344624ff109501055a912b60
asciisummer <romelsoviguidi@gmail.com>
2024-06-06 18:07:05 +0100

    fix: reverse list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,9 @@
 
 def print_reversed_list_integer(my_list=[]):
     tmp = my_list[:]
-    for i in tmp.reverse():
+    tmp.reverse()
+    for i in tmp:
         print("{:d}".format(i))
+
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
