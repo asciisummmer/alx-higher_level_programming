8d224136125c7f10c50f63b590107bb6154e4f20
asciisummer <romelsoviguidi@gmail.com>
2024-06-20 00:45:16 +0100

    add method to class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let line = '';
+    for (let i = 1; i <= this.width; i++) {
+      line += 'X';
+    }
+    for (let i = 1; i <= this.height; i++) {
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+module.exports = Rectangle;
