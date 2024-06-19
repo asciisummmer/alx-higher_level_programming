42d7aa2da96c6167181050c220b5207ced9d5f2d
asciisummer <romelsoviguidi@gmail.com>
2024-06-19 01:32:09 +0100

    fix error is integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (/^\d+$/.test(argv[2])) {
-  console.log('My number: ' + argv[2]);
+if (!isNaN(Number(argv[2]))) {
+  console.log('My number: ' + parseInt(argv[2]));
 } else {
   console.log('Not a number');
 }
