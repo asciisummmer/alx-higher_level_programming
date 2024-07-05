400d531b8af34dc854ba506a234eb1456828f49e
asciisummer <romelsoviguidi@gmail.com>
2024-07-05 23:49:46 +0100

    fix: conditions for script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,11 @@
 
 from urllib import request
 
-req = request.Request('https://alx-intranet.hbtn.io/status')
-with request.urlopen(req) as response:
-    body = response.read()
-    print("Body response:")
-    print(f"\t- type: {type(body)}")
-    print(f"\t- content: {body}")
-    print(f"\t- utf8 content: {body.decode()}")
+if __name__ == "__main__":
+    req = request.Request('https://alx-intranet.hbtn.io/status')
+    with request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode()}")
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,7 @@ import sys
 from urllib import request
 
 
-req = request.Request(sys.argv[1])
-with request.urlopen(req) as response:
-    print(response.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    req = request.Request(sys.argv[1])
+    with request.urlopen(req) as response:
+        print(response.headers.get("X-Request-Id"))
