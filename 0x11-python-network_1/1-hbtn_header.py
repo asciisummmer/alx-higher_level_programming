188707bc3f6d2a2099c0bc7a12937825e5e13ca0
asciisummer <romelsoviguidi@gmail.com>
2024-07-05 23:21:36 +0100

    fix: import syntax

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Get info from http header
 """
+
 import sys
-import urllib.request
+from urllib import request
 
-request = urllib.request.Request(sys.argv[1])
+req = request.Request(sys.argv[1])
 
-with urllib.request.urlopen(request) as response:
+with request.urlopen(req) as response:
     for header_info in response.headers.items():
         if header_info[0] == "X-Request-Id":
             print(header_info[1])
