c74f3ea41323a755a425df8c864958336621cb8d
asciisummer <romelsoviguidi@gmail.com>
2024-07-06 00:42:06 +0100

    fix: decode encodage

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,9 +11,10 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     value = {"email": email}
-    data = urllib.parse.urlencode(value)
+    data = parse.urlencode(value)
     data = data.encode('ascii')
 
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode())
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode())
