4f5b72734ff3e482b57ca9fd953e20129bc81777
asciisummer <romelsoviguidi@gmail.com>
2024-07-05 23:43:49 +0100

    get info headers by method get

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,13 +2,10 @@
 """ Get info from http header
 """
 
-
 import sys
 from urllib import request
 
 
 req = request.Request(sys.argv[1])
 with request.urlopen(req) as response:
-    for header_info in response.headers.items():
-        if header_info[0] == "X-Request-Id":
-            print(header_info[1])
+    print(response.headers.get("X-Request-Id"))
