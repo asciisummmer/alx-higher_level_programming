b0c6eec2a212fc4f116b18e6aeaa313ee8bf0b96
asciisummer <romelsoviguidi@gmail.com>
2024-07-05 21:06:47 +0100

    fetch data

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" fetch data from ressource 
+"""
+
+from urllib import request
+
+req = request.Request('https://alx-intranet.hbtn.io/status')
+with request.urlopen(req) as response:
+    body = response.read()
+    print("Body response")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode()}")
