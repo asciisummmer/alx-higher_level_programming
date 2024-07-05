4b8f723c82e7882cca571a554e23cb811f3ae6c1
asciisummer <romelsoviguidi@gmail.com>
2024-07-05 23:36:54 +0100

    fix: coding style

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,8 +5,8 @@
 import sys
 from urllib import request
 
-req = request.Request(sys.argv[1])
 
+req = request.Request(sys.argv[1])
 with request.urlopen(req) as response:
     for header_info in response.headers.items():
         if header_info[0] == "X-Request-Id":
