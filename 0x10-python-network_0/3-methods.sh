c68b771b65e718ddd87d505d06ea86712839750f
asciisummer <romelsoviguidi@gmail.com>
2024-07-05 02:07:56 +0100

    set header http request

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # display allow method
-curl -s --head 0.0.0.0:5000/route_4 | grep -i "Allow" | cut -d ":" -f 2 | xargs
+curl -s --head 0.0.0.0:5000/route_4 $1 | grep -i "Allow" | cut -d ":" -f 2 | xargs
