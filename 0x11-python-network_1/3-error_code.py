33c2f56a4a72d90c7c24e96e350b4e9e8bdb6ae6
asciisummer <romelsoviguidi@gmail.com>
2024-07-06 02:15:18 +0100

    handle error url

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,9 +8,8 @@ from urllib import request, error
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    req = request.Request(url)
     try:
-        with request.urlopen(req) as response:
+        with request.urlopen(url) as response:
             body = response.read()
             print(body.decode('UTF-8'))
     except error.HTTPError as e:
