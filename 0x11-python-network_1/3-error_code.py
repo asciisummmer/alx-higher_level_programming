6d9ea8823582adcfdc0c220a71d544c354dc8030
asciisummer <romelsoviguidi@gmail.com>
2024-07-06 01:41:14 +0100

    handle error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
             body = response.read()
             print(body.decode())
     except error.HTTPError as e:
-        print(f"Error code:{e.code}")
+        print("Error code:", e.code)
