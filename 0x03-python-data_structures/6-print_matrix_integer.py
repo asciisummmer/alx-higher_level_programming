e5b66b8d2f0c825a24a6a15b113c5ee896048146
asciisummer <romelsoviguidi@gmail.com>
2024-07-24 18:40:31 +0100

    display matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    res = ""
+    for row in matrix:
+        len_of_row = len(row)
+        for i in range(len_of_row):
+            print("{:d}{}".format(row[i], " " if i + 1 != len_of_row else ""), end="")
+        print()
