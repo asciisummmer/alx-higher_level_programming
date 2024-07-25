ff6fe7b8df9f364a88de4e1f593660d217935ddd
asciisummer <romelsoviguidi@gmail.com>
2024-07-25 09:49:57 +0100

    fix pycodestyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,10 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         len_of_row = len(row)
         for i in range(len_of_row):
-            print("{:d}{}".format(row[i], " " if i + 1 != len_of_row else ""), end="")
+            print(
+                "{:d}{}".format(
+                    row[i], " "
+                    if i + 1 != len_of_row else ""
+                    ), end=""
+                )
         print()
