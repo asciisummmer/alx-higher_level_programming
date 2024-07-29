865d4906719d8f900fb9b72b5b0b8efdf743517a
asciisummer <romelsoviguidi@gmail.com>
2024-07-29 10:43:53 +0100

    print sorted dict

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_dict_keys = sorted(a_dictionary)
+
+    for key in sorted_dict_keys:
+        print(f"{key}: {a_dictionary[key]}")
