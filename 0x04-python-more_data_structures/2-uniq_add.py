25f8e341a52cccacf49223f1365d29411e63b605
asciisummer <romelsoviguidi@gmail.com>
2024-07-29 10:20:27 +0100

    uniq add

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    new_unique_list = []
+    for elt in my_list:
+        if elt not in new_unique_list:
+            new_unique_list.append(elt)
+    return new_unique_list
