f31488dd1b3f3e26752743acb7defbfdf4a241bd
asciisummer <romelsoviguidi@gmail.com>
2024-08-08 23:30:38 +0100

    fix empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ from functools import reduce
 
 
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
+    if len(my_list) == 0 or my_list is None:
         return 0
     num = reduce(lambda x, y: (x[0] * x[1] + y[0] * y[1], 1), my_list)
     denom = reduce(lambda x, y: (0, x[1] + y[1]), my_list)
