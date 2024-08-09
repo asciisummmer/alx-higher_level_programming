d81f47e4d83b4ba31a15ccb02087467ef2cf6791
asciisummer <romelsoviguidi@gmail.com>
2024-08-09 01:26:49 +0100

    remove comment

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,6 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(0, x):
         try:
-           # if isinstance(my_list[i], int):
             print("{:d}".format(my_list[i]),  end="")
             count += 1
         except (TypeError, ValueError):
