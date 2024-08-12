f4ebf8aba0f30f19686736ed8488fb613b0104e6
asciisummer <romelsoviguidi@gmail.com>
2024-08-12 19:05:08 +0100

    handle TypeError

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as err:
+        return True
+    except (ValueError, TypeError) as err:
         sys.stderr.write(f"Exception: {err}\n")
         return False
-    return True
