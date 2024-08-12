be5a430dd33af675169cb44ab8e747240c9c57a0
asciisummer <romelsoviguidi@gmail.com>
2024-08-12 18:24:41 +0100

    safe print

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as err:
+        sys.stderr.write(f"Exception: {err}\n")
+        return False
+    return True
