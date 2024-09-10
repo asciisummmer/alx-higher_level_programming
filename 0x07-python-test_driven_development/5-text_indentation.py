4d3056f80bbf97771c255a70feba33faa1355e23
asciisummer <romelsoviguidi@gmail.com>
2024-09-10 22:47:45 +0100

    fix some errors

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,15 +12,9 @@ def text_indentation(text):
 
     if type(text) is not str:
         raise TypeError("text must be a string")
-    text_translation = str.maketrans({
-        '.': '.\n\n', '?': '?\n\n', ':': ':\n\n'
-        })
-    translated_text = text.translate(text_translation)
-    text_translation = str.maketrans(
-        {
-            '.\n\n ': '.\n\n', '?\n\n ': '?\n\n',
-            ':\n\n ': ':\n\n'
-        }
-    )
-    translated_text = translated_text.translate(text_translation)
+
+    translated_text = text.replace("? ", "?\n\n")
+    translated_text = translated_text.replace(". ", ".\n\n")
+    translated_text = translated_text.replace(": ", ":\n\n")
+
     print(translated_text)
