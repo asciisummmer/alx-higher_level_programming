ef5ce48dd35e69657ae00bb5a0cf2264487deb73
asciisummer <romelsoviguidi@gmail.com>
2024-09-10 16:20:21 +0100

    fix coding style

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,6 +5,7 @@
     Module to describe how to divide matrix
 """
 
+
 def matrix_divided(matrix, div):
     """
     function to divide matrix by integer
@@ -12,21 +13,28 @@ def matrix_divided(matrix, div):
     """
     result = []
     new_line = []
-    if type(matrix) != list or type(matrix[0]) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(matrix) is not list or type(matrix[0]) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+            )
     size_row = len(matrix[0])
-    if type(div) != int and type(div) != float:
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     for line in matrix:
-        if type(line) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if (len(line) != size_row):
+        if type(line) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
+        if (len(line) is not size_row):
             raise TypeError("Each row of the matrix must have the same size")
         for cel in line:
-            if type(cel) != int and type(cel) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            if type(cel) is not int and type(cel) is not float:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) \
+                    of integers/floats"
+                    )
             new_line.append(round(cel / div, 2))
         result.append(new_line.copy())
         new_line.clear()
