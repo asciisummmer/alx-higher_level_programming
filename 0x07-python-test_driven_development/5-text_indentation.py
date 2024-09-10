f58749e747b684dce275a06831c4ecd0c8b7925c
asciisummer <romelsoviguidi@gmail.com>
2024-09-10 21:18:03 +0100

    transform text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Module supplies function print text.
+    Module to describe how to print text.
+"""
+
+
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    text_translation = str.maketrans({'.': '.\n\n', '?': '?\n\n', ':': '\n\n'})
+    translated_text = text.translate(text_translation)
+    print(translated_text)
