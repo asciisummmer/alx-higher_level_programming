0fa01fb34d03fd3582b021518fbf9648b470380d
asciisummer <romelsoviguidi@gmail.com>
2024-09-17 13:51:21 +0100

    fix error add item not  created

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,6 +15,12 @@ if __name__ == "__main__":
     if len(args) == 0:
         with open(filename, "w", encoding="utf-8") as f:
             json.dump([], f)
-    value = load_from_json_file(filename)
+        exit(0)
+    try:
+        value = load_from_json_file(filename)
+    except FileNotFoundError:
+        with open(filename, "w", encoding="utf-8") as f:
+            value = []
+            json.dump(value, f)
     value += args
     save_to_json_file(value, filename)
