a98aec2f8bae6c97e5e9cb526d60e32dc5842630
asciisummer <romelsoviguidi@gmail.com>
2024-09-19 15:14:25 +0100

    def rectangle class with getter and setter

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """ Module to describe base of model
 
 """
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,46 @@
+""" Module to define Rectangle
+
+"""
+
+from .base import Base
+
+
+class Rectangle(Base):
+    """Class to define rectangle
+
+    Args:
+        width (int): width of rectangle
+        height (int): height of rectangle
+        x (int): position of rectangle
+        y (int): position of rectangle
+
+    Attributes:
+        width (int): width of rectangle
+        height (int): height of rectangle
+        x (int): position of rectangle
+        y (int): position of rectangle
+
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width():
+        return self.__width
+
+    @property
+    def height():
+        return self.__height
+
+    @width.setter
+    def width(w):
+        self.__width = w
+
+    @height.setter
+    def height(h):
+        self.__height = h
