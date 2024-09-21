d521404e8174be10f0aa317c646807bb0b11cbb8
asciisummer <romelsoviguidi@gmail.com>
2024-09-21 18:44:02 +0100

    dict to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 
 """
 
+import json
+
 
 class Base:
     """Class to define base of my class
@@ -23,3 +25,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
